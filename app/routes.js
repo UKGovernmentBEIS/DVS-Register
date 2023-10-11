8fc8ebec3aadd5808915b2d8114df9679a462c6f
vishalvishwanathan2 <vishal.vishwanathan@beis.gov.uk>
2023-10-11 14:10:30 +0100

    Routing setup, along with versioning. Versioning defaulted to v1 for now

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,26 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+const version = 'v1';
+
+router.get('/', function(req, res){
+    res.redirect('/'+version+'/public-facing-register/start');
+});
+router.get('/'+version, function(req, res){
+    res.redirect('/'+version+'/public-facing-register/start');
+});
+
+router.get('/search', function(req, res){
+    res.redirect('/'+version+'/public-facing-register/search');
+});
+
+router.get('/company1', function(req, res){
+    res.redirect('/'+version+'/public-facing-register/company1');
+});
+
+
+// Temporary Routing for the IDSP Onboarding, full journey will be fleshed out
+router.get('/'+version+'/idsp-onboarding', function(req, res){
+    res.redirect('/'+version+'/idsp-onboarding/application-portal/start');
+});
